201fd88df8b8c778d85f58db15b61cff0b40c6b7
Miracl0ve <patelmira2@myvuw.ac.nz>
2024-09-12 11:53:40 +1200

    Adding saturn image!

--- music_starter.js
+++ music_starter.js
@@ -1,32 +1,36 @@
-
+let img;
+let firstRun = true
 let circleRotate = 0;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-
+  if(firstRun){
+    img = loadImage('clipart610239.png') //https://www.clipartmax.com/download/m2i8H7m2G6G6Z5A0_saturn-transparent/
+    firstRun = false
+}
+  
 
   background(0)
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
-  let img;
-  let firstRun = true
-
-  
-function draw_one_frame (word, vocal, drum, bass, other, counter){
-  if(firstRun){
-    img = loadImage('clipart610239.png') //https://www.clipartmax.com/download/m2i8H7m2G6G6Z5A0_saturn-transparent/
-    firstRun = false
-}
-image (img, 640, 360);
-}
 
 
-fill (38, 4, 79)
-  ellipse (640, 360, counter ** 2 / 100, counter ** 2 / 100)
-  fill(31, 17, 51)
- ellipse (640, 360, counter ** 1 , counter ** 1)
- fill(66, 5, 8)
+
+  if (counter > 200 && counter < 700) {
+    push();
+    translate(100, 100)
+    scale(0.5)
+    image (img, 0, 0);
+    pop();
+  }
+
+
+//fill (38, 4, 79)
+  //ellipse (640, 360, counter ** 2 / 100, counter ** 2 / 100)
+  //fill(31, 17, 51)
+// ellipse (640, 360, counter ** 1 , counter ** 1)
+ //fill(66, 5, 8)
  //ellipse (640, 360, counter *1, counter *1)
 //fill (42, 1, 97)
 //ellipse (640, 360, counter * 3 / 30, counter * 3 / 30)
@@ -36,33 +40,6 @@ fill (38, 4, 79)
  //let ellipseSize = map(drum, 10, 50, 30, 70)
 
 
- //fill(194, 78, 54)
- //let circle2Size = map(counter, 0, 40, 50, 20);
- //ellipse(500, 650, 50, circle2Size)
- //fill(124, 2, 245)
- //ellipse(500, 500, circle2Size,circle2Size)
- //fill(194, 78, 54)
- //ellipse(500, 350, 50,circle2Size)
- //fill(124, 2, 245)
- //ellipse(500, 200, circle2Size,circle2Size)
- //fill(194, 78, 54)
- //ellipse(500, 50, 50,circle2Size)
-
- fill(130, 31, 12)
- let circleSize = map(counter, 0, 80, 60, 20);
- ellipse(640, 650, 60, circleSize)
- fill(87, 2, 179)
- ellipse(640, 500, circleSize,circleSize)
- fill(130, 31, 12)
- ellipse(640, 350, 60,circleSize)
- fill(87, 2, 179)
- ellipse(640, 200, circleSize,circleSize)
- fill(130, 31, 12)
- ellipse(640, 50, 60,circleSize)
-
-
- 
-
  //fill(94, 204, 204)
  //push()
  //translate(640, 400)
@@ -70,21 +47,20 @@ fill (38, 4, 79)
 //ellipse(0, 0, 150, 40);
 //pop()
 
-push()
-translate(640,360)
-rotate(circleRotate)
-circleRotate += 2;
-fill(216, 232, 232)
-ellipse(0, 0, 100, 20);
-pop()
-
-if (counter > 400 && counter < 700) {
-
-}
+// push()
+// translate(640,360)
+// rotate(circleRotate)
+// circleRotate += 2;
+// fill(216, 232, 232)
+// ellipse(0, 0, 100, 20);
+// pop()
 
 
 
-fill(177, 165, 194)
+
+
+
+//fill(177, 165, 194)
  //beginShape();
  //vertex(609, 501);
  //bezierVertex(map(bass, 20, 100, 50, 250), 700, 690, 310, 879, 363);
@@ -101,8 +77,8 @@ fill(177, 165, 194)
 //endShape();
 }
 
-function introEllipse(channel, col, minMap, maxMap) {
-}
+// function introEllipse(channel, col, minMap, maxMap) {
+// }
 
 
 
