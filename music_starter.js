5cbe47501bc12a96f2973378a53f5fc9f3eeeada
Miracl0ve <patelmira2@myvuw.ac.nz>
2024-09-15 10:12:06 +1200

    adding bass channel/some changes

--- music_starter.js
+++ music_starter.js
@@ -1,11 +1,11 @@
 let img;
 let firstRun = true
 let circleRotate = 1;
-//let drumMap = map(drum, 0, 100, 15, 40);
-
+//let imageRotate = 1;
 let angle = 0;
 let r = 200;
 let vocal_history = [];
+
 
 function add_to_history(history, d) {
   history.push(d);
@@ -17,7 +17,7 @@ function add_to_history(history, d) {
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if(firstRun){
-    img = loadImage('clipart610239.png') //https://www.clipartmax.com/download/m2i8H7m2G6G6Z5A0_saturn-transparent/
+    img = loadImage('moon-png-44673.png') //https://www.clipartmax.com/download/m2i8H7m2G6G6Z5A0_saturn-transparent/
     firstRun = false
 }
 
@@ -26,57 +26,72 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
 
-  if(counter > 335 && counter < 8000){
-    stroke (255, 255, 255)
-    strokeWeight(5);
-    fill(0, 0, 0)
-     push()
-     translate(300,230)
-    rotate(circleRotate)
-    circleRotate += 50;
-    //fill(7, 10, 31)
-    strokeWeight(2);
-    ellipse (50, 40, 50, 800)
-    ellipse (50, 50, 100, 700)
-    ellipse (50, 40, 150, 600)
+
+let circleSize = map(bass, 0, 80, 40, 200);
+stroke(127, 126, 191)
+fill(127, 126, 191)
+
+
+
+//let ellipseSize = map(drum, 10, 50, 30, 70)
+
+//fill(65, 14, 120);
+//ellipse(300, 250, ellipseSize, ellipseSize);
+//fill(125, 109, 140)
+//ellipse(300, 250, 20, ellipseSize);
+
+
+//push()
+//if (counter > 335 && counter < 1020) {
+ // translate(100, 100)
+  //scale(0.2)
+  //image (img, 0, 0);
+//}
+//pop()
+
+
+push()
+if(counter > 340 && counter < 8000){
+  stroke (255, 255, 255)
+  strokeWeight(1);
+  fill(0, 0, 0)
+   translate(350, 200)
+  rotate(circleRotate)
+  circleRotate += 0.01;
+  //fill(7, 10, 31)
+  ellipse (45, 50, 50, 800)
+  //stroke (65, 11, 122);
+  ellipse (45, 45, 100, 700)
+  stroke(255)
+  ellipse (45, 45, 150, 600)
+  //stroke (65, 11, 122);
+    ellipse(45, 45, 200, 500);
+    //fill(120, 87, 156);
+    stroke(255);
+    ellipse(45, 45, 250, 250);
+    //fill(0);
+fill(127, 126, 191)
     stroke (65, 11, 122);
-    strokeWeight(2);
-      ellipse(50, 50, 200, 500);
-      //fill(120, 87, 156);
-      stroke(255);
-      strokeWeight(2);
-      ellipse(50, 40, 250, 400);
-      //fill(0);
-      stroke (65, 11, 122);
-      strokeWeight(2);
-      ellipse(50, 50, 300, 300);
-      fill(0)
-      ellipse(50, 40, 250, 200)
-      //fill(120, 87, 156);
-      stroke (65, 11, 122);
-      strokeWeight(2);
-      ellipse(50, 50, 200, 200);
-      stroke(255)
-      ellipse(50, 40, 150, 150)
-      fill(0);
-      stroke (65, 11, 122);
-     ellipse (50, 50, 100, 100);
-     stroke (255);
-     fill(66, 29, 196)
-    ellipse(50, 40, 50, 50);
-    pop()
-    }
-  
+    ellipse(45, 45, circleSize, circleSize);
+    stroke(255)
+    fill(168, 167, 232)
+    ellipse (45, 45, 30, 30)
+    
+   
+  }
+  pop()
+
+push()
 angleMode (RADIANS)
 add_to_history(vocal_history, vocal);
-  translate(300, 250);
-  strokeWeight(7);
-  stroke(175, 218, 224);
+  translate(310, 250);
+  strokeWeight(4);
+  stroke(185, 161, 237);
   //r = vocal
-for(let i = 0; i < 20; i++){
+for(let i = 20; i <= 40; i++){
   historyVal = vocal_history[vocal_history.length - i]
-r = map(historyVal, 0, 20, 10, 40)
-  let angle = map(i, 10, 20, 0, PI*3)
+r = map(historyVal, 30, 10, 10, 40)
+  let angle = map(i, 10, 20, 10, PI*6)
   let x = r * cos(angle);
   let y = r * sin(angle);
   point(x, y);
@@ -84,20 +99,8 @@ r = map(historyVal, 0, 20, 10, 40)
   angle += 0.05; //random(-0.1, 0.1);
   r -= random(1, 1);
 }
+pop()
 
- 
-  //if (counter > 335 && counter < 10000) {
-   // push();
-   // translate(140, 150)
-   // scale(0.5)
-   //image (img, 0, 0);
-   //fill(4, 12, 74);
-   // pop();
-  //}
-
-//stroke(255);
-//let starRays = map(bass, 20, 85, 75);
-//line(30, 20,drum, 75);
 
 //for (let i = 1; i <= 10; i ++){
 //ellipse()
@@ -139,6 +142,13 @@ r = map(historyVal, 0, 20, 10, 40)
 //pop()
 
 
+
+//push()
+//stroke(255);
+//strokeWeight(2)
+//let starRays = map(bass, 10, 85, 75);
+//line(30, 20, 100, 105);
+//pop()
 
 
 
